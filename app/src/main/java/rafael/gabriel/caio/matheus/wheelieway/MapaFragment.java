55b4e25b5745fd618ac1dfa757ea23a50a269b2a
dwqdw <ifrn.jucurutu@gmail.com>
2023-06-16 09:59:37 -0300

    P.8 bottom view

--- app/src/main/java/rafael/gabriel/caio/matheus/wheelieway/MapaFragment.java
+++ app/src/main/java/rafael/gabriel/caio/matheus/wheelieway/MapaFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ListViewFragment#newInstance} factory method to
+ * Use the {@link MapaFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ListViewFragment extends Fragment {
+public class MapaFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class ListViewFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public ListViewFragment() {
+    public MapaFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class ListViewFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment ListViewFragment.
+     * @return A new instance of fragment MapaFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ListViewFragment newInstance(String param1, String param2) {
-        ListViewFragment fragment = new ListViewFragment();
+    public static MapaFragment newInstance(String param1, String param2) {
+        MapaFragment fragment = new MapaFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class ListViewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_view, container, false);
+        return inflater.inflate(R.layout.fragment_mapa, container, false);
     }
 }
